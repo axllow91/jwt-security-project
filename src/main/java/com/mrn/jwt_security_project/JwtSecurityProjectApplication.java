ebad138fb14a81f02b8a694d7e49208907cb1892
Marian Dobre <dobremi91@gmail.com>
2020-10-09 18:42:28 +0300

    Added new methods in the user service (create,update,delete). Created the methods for us to set and update profile image for user.

--- src/main/java/com/mrn/jwt_security_project/JwtSecurityProjectApplication.java
+++ src/main/java/com/mrn/jwt_security_project/JwtSecurityProjectApplication.java
@@ -5,11 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.io.File;
+
+import static com.mrn.jwt_security_project.constant.FileConstant.USER_FOLDER;
+
 @SpringBootApplication
 public class JwtSecurityProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JwtSecurityProjectApplication.class, args);
+        new File(USER_FOLDER).mkdirs();
     }
 
     @Bean
