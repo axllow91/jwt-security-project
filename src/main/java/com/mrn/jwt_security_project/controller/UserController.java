489d1d8cc2f22377721af16a932bb47be473e0cc
Marian Dobre <dobremi91@gmail.com>
2020-10-13 10:03:11 +0300

    Added functionality for the user to add, update, find, delete (if is a super admin), reset password, get and update profile image and temp profile image.

--- src/main/java/com/mrn/jwt_security_project/controller/UserController.java
+++ src/main/java/com/mrn/jwt_security_project/controller/UserController.java
@@ -1,30 +1,44 @@
 package com.mrn.jwt_security_project.controller;
 
+import com.mrn.jwt_security_project.domain.HttpResponse;
 import com.mrn.jwt_security_project.domain.User;
 import com.mrn.jwt_security_project.domain.UserPrincipal;
 import com.mrn.jwt_security_project.exception.ExceptionHandling;
 import com.mrn.jwt_security_project.exception.domain.EmailExistException;
+import com.mrn.jwt_security_project.exception.domain.EmailNotFoundException;
 import com.mrn.jwt_security_project.exception.domain.UserNotFoundException;
 import com.mrn.jwt_security_project.exception.domain.UsernameExistException;
 import com.mrn.jwt_security_project.service.UserService;
 import com.mrn.jwt_security_project.utility.JWTTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
+import static com.mrn.jwt_security_project.constant.FileConstant.*;
 import static com.mrn.jwt_security_project.constant.SecurityConstant.JWT_TOKEN_HEADER;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
 import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.MediaType.IMAGE_JPEG_VALUE;
 
 @RestController
-@RequestMapping(path = { "/", "/user"})
+@RequestMapping(path = {"/", "/user"})
 public class UserController extends ExceptionHandling {
+    public static final String EMAIL_SENT = "An email with a new password: ";
+    public static final String USER_DELETED_SUCCESSFULLY = "User deleted successfully!";
     private AuthenticationManager authenticationManager;
     private UserService userService;
     private JWTTokenProvider jwtTokenProvider;
@@ -51,6 +65,129 @@ public class UserController extends ExceptionHandling {
         return new ResponseEntity<>(loginUser, jwtHeader, OK);
     }
 
+    @PostMapping("/add")
+    public ResponseEntity<User> addNewUser(@RequestParam("firstName") String firstName,
+                                           @RequestParam("lastName") String lastName,
+                                           @RequestParam("username") String username,
+                                           @RequestParam("email") String email,
+                                           @RequestParam("role") String role,
+                                           @RequestParam("isActive") String isActive,
+                                           @RequestParam("isNonLocked") String isNonLocked,
+                                           @RequestParam(value = "profileImage", required = false) MultipartFile profileImage) throws UserNotFoundException, UsernameExistException, EmailExistException, IOException {
+
+        User newUser = userService.addNewUser(firstName, lastName, username, email, role,
+                Boolean.parseBoolean(isActive),
+                Boolean.parseBoolean(isNonLocked),
+                profileImage
+        );
+
+        return new ResponseEntity<>(newUser, OK);
+
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<User> updateUser(
+            @RequestParam("currentUsername") String currentUsername,
+            @RequestParam("firstName") String firstName,
+            @RequestParam("lastName") String lastName,
+            @RequestParam("username") String username,
+            @RequestParam("email") String email,
+            @RequestParam("role") String role,
+            @RequestParam("isActive") String isActive,
+            @RequestParam("isNonLocked") String isNonLocked,
+            @RequestParam(value = "profileImage", required = false) MultipartFile profileImage) throws UserNotFoundException, UsernameExistException, EmailExistException, IOException {
+
+        User updatedUser = userService.updateUser(currentUsername, firstName, lastName, username, email, role,
+                Boolean.parseBoolean(isActive),
+                Boolean.parseBoolean(isNonLocked),
+                profileImage
+        );
+
+        return new ResponseEntity<>(updatedUser, OK);
+
+    }
+
+    @GetMapping("/find/{username}")
+    public ResponseEntity<User> getUser(@PathVariable("username") String username) {
+
+        User user = userService.findUserByUsername(username);
+        return new ResponseEntity<>(user, OK);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<User>> getAllUsers() {
+
+        List<User> users = userService.getUsers();
+        return new ResponseEntity<>(users, OK);
+    }
+
+    @GetMapping("/resetPassword/{email}")
+    public ResponseEntity<HttpResponse> resetPassword(@PathVariable("email") String email) throws EmailNotFoundException {
+        userService.resetPassword(email);
+        return response(OK, EMAIL_SENT + email);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @PreAuthorize("hasAuthority('user:delete')")
+    // it means that for you to delete you need to be a super user (admin), see Authority class
+    public ResponseEntity<HttpResponse> deleteUser(@PathVariable("id") Long id) {
+
+        userService.deleteUser(id);
+        return response(NO_CONTENT, USER_DELETED_SUCCESSFULLY);
+    }
+
+    @PostMapping("/updateProfileImage")
+    public ResponseEntity<User> updateProfileImage(
+            @RequestParam("username") String username,
+            @RequestParam(value = "profileImage") MultipartFile profileImage)
+            throws UserNotFoundException, UsernameExistException, EmailExistException, IOException {
+
+        User user = userService.updateProfileImage(username, profileImage);
+
+        return new ResponseEntity<>(user, OK);
+
+    }
+
+    private ResponseEntity<HttpResponse> response(HttpStatus httpStatus, String message) {
+        return new ResponseEntity<>(
+                new HttpResponse(httpStatus.value(), httpStatus, httpStatus.getReasonPhrase().toUpperCase(), message.toUpperCase()), // -> this is body see HttpResponse class that we created
+                httpStatus);
+    }
+
+    // image returns an array of bytes
+    @GetMapping(path = "/image/{username}/{fileName}", produces = IMAGE_JPEG_VALUE)
+    public byte[] getProfileImage(@PathVariable("username") String username, @PathVariable("fileName") String fileName) {
+        try {
+            return Files.readAllBytes(Paths.get(USER_FOLDER + username + FORWARD_SLASH + fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @GetMapping(path = "/image/{profile}/{username}", produces = IMAGE_JPEG_VALUE)
+    public byte[] getTempProfileImage(@PathVariable("username") String username) {
+
+        URL url = null; // https://robobash.org/{username}
+
+        try {
+            url = new URL(TEMP_PROFILE_IMAGE_BASE_URL + username);
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            InputStream inputStream = url.openStream();
+            int bytesRead;
+            byte[] chunk = new byte[1024]; // 10 MB
+            while ((bytesRead = inputStream.read(chunk)) > 0) {
+                byteArrayOutputStream.write(chunk, 0, bytesRead);
+            }
+            return byteArrayOutputStream.toByteArray();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
     private HttpHeaders getJwtHeader(UserPrincipal user) {
         HttpHeaders headers = new HttpHeaders();
         headers.add(JWT_TOKEN_HEADER, jwtTokenProvider.generateJwtToken(user));
@@ -60,4 +197,6 @@ public class UserController extends ExceptionHandling {
     private void authenticate(String username, String password) {
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
     }
+
+
 }
